ada93ef5af31e26843ab3cbeb3d997164aca261f
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-06 15:10:43 +0700

    Menampilkan data satu per satu

--- Array.cpp
+++ Array.cpp
@@ -27,3 +27,11 @@ int main() {
     cin >> kamu[3];
     out << "Data index 4 = ";
     cin >> kamu[4]
+
+   // Menampilkan Satu per Satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << "Data Kelima = " << kamu[4] << endl;
+    cout << endl;
