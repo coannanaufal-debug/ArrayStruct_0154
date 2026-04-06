2f86bd725595a566e78c5c7c509b3b7a6988ae81
coannanaufal-debug <coanna.naufal@gmail.com>
2026-04-06 15:40:41 +0700

    menambah detail alamt

---
+++ StructNew2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "alamat kota :";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat.desa << endl;
+    cout << " Alamat : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
